103ccf8876e3cf969861b8b851f64823a75138a1
febin26 <ishaalfebinu23sit@vetias.ac.in>
2025-05-13 11:31:16 +0530

    Add files via upload

---
+++ Characters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  char arr[20]="happy";
+  int y=15;
+  char x[15]={'e','n','d','i','n','g','\0'};
+  printf("%c",x[3]);
+  printf("\n%s\n",arr);
+  printf("%s",x);
+
+    return 0;
+}
